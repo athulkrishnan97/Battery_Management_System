b5a4c36d877de576599361f4eff7ae104b628d49
Francina Rachel Thomas <118345129+francinarachel@users.noreply.github.com>
2022-11-26 03:28:12 +0530

    Update Simulator.java

--- app/src/main/java/com/project/batterymanagementsystem/simulation/Simulator.java
+++ app/src/main/java/com/project/batterymanagementsystem/simulation/Simulator.java
@@ -10,23 +10,25 @@ public class Simulator {
     public void setChangeListener(SimulationListener listener) {
         this.mListener = listener;
     }
+    
+    //To simulate working logic of the system and simulate temperature sensor and voltmeter readings
 
     public void simulateData(int updateDelay, double temp, int health){
         while (true) {
             try {
-                while (temp < 33) { // Fast charge
-                    temp = temp + Math.random();
+                while (temp < 33) { // Logic to simulate Fast charge
+                    temp = temp + Math.random(); // To simulate temperature sensor readings
                     Thread.sleep(updateDelay);
                     if (mListener != null)
                         mListener.onStatusChange(temp, 15, health, ChargeType.FAST,1);
                 }
-                while (temp > 33 && temp < 35) { //Average Charging
+                while (temp > 33 && temp < 35) { //Logic to simulate Average Charging
                     temp = temp + (Math.random() / 2);
                     Thread.sleep(updateDelay);
                     if (mListener != null)
                         mListener.onStatusChange(temp, 9, health, ChargeType.AVERAGE,0.5);
                 }
-                while (temp > 27) { //Slow Charging
+                while (temp > 27) { //Logic to simulate Slow Charging
                     temp = temp - Math.random();
                     Thread.sleep(updateDelay);
                     if (mListener != null)
